91b311835090926451049e200a721c67397b2599
Rojta <63408233+Rojta@users.noreply.github.com>
2021-12-28 16:34:15 +0100

    pwa

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,10 @@
 module.exports = {
+    pwa: {
+        name: 'Belote Helper',
+        themeColor: '#fc003f',
+        msTileColor: '#000000',
+        appleMobileWebAppCapable: 'yes',
+        appleMobileWebAppStatusBarStyle: 'black',
+    },
     publicPath: '/belote-helper/'
 }
