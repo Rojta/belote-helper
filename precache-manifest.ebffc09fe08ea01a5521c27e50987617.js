dec73b4ae16d36cf771a356d8832273665c925d2
Rojta <63408233+Rojta@users.noreply.github.com>
2021-12-31 15:48:40 +0100

    New Deployment

---
+++ precache-manifest.ebffc09fe08ea01a5521c27e50987617.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d3a392ce551d7d10f2e",
+    "url": "/belote-helper/css/app.6393e9f7.css"
+  },
+  {
+    "revision": "0e82b00335bd55d99d08",
+    "url": "/belote-helper/css/chunk-vendors.eae9c2bb.css"
+  },
+  {
+    "revision": "943c3597cd33be56d53df0d1982fa8ff",
+    "url": "/belote-helper/fonts/primeicons.943c3597.woff"
+  },
+  {
+    "revision": "b29a888ff7f07091c7e08eb0d991e221",
+    "url": "/belote-helper/fonts/primeicons.b29a888f.ttf"
+  },
+  {
+    "revision": "e01fd4133bac49cd2ea07ad6f7c45695",
+    "url": "/belote-helper/fonts/primeicons.e01fd413.eot"
+  },
+  {
+    "revision": "c7a33805ffda0d32bd2a9904c8b02750",
+    "url": "/belote-helper/img/color.c7a33805.png"
+  },
+  {
+    "revision": "c832e17498b0ff5b1cef0e666bdc61eb",
+    "url": "/belote-helper/img/logo.c832e174.png"
+  },
+  {
+    "revision": "64b5d470af63a67aa9b9d3e8a866a35e",
+    "url": "/belote-helper/img/primeicons.64b5d470.svg"
+  },
+  {
+    "revision": "28610f95e0a22b30208421ce65a71f76",
+    "url": "/belote-helper/index.html"
+  },
+  {
+    "revision": "0d3a392ce551d7d10f2e",
+    "url": "/belote-helper/js/app.8c3cd4bc.js"
+  },
+  {
+    "revision": "0e82b00335bd55d99d08",
+    "url": "/belote-helper/js/chunk-vendors.b23dcfce.js"
+  },
+  {
+    "revision": "7717292811d89c1f2e2321c672a13102",
+    "url": "/belote-helper/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/belote-helper/robots.txt"
+  },
+  {
+    "revision": "8efeda5b2d8e09aa545b49561bcf1d00",
+    "url": "/belote-helper/themes/dark.css"
+  },
+  {
+    "revision": "f09a2d142ef02ec9e0a2ad53f2a82917",
+    "url": "/belote-helper/themes/light.css"
+  }
+]);
